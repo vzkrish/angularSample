29894c0f1e921d7e5fff0e9d67f62f75ba498e6d
vzkrish <krajubraju@gmail.com>
2018-08-23 18:06:47 +0530

    Add routing options to Existing food Hub application

--- foodhub/src/app/app-routing.module.ts
+++ foodhub/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'hotels', component: RestaurantComponent},
+  {path: '**', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
