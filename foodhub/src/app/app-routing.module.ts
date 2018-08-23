3135ebacad7be75df12685aaf7a0892a51644208
vzkrish <krajubraju@gmail.com>
2018-08-23 18:20:45 +0530

    Updating routing in the html and configuring with the route components

--- foodhub/src/app/app-routing.module.ts
+++ foodhub/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RestaurantComponent } from './restaurant/restaurant.component';
+import { OrderhistoryComponent } from './orderhistory/orderhistory.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'hotels', component: RestaurantComponent},
+  {path: 'history', component: OrderhistoryComponent},
   {path: '**', component: LoginComponent}
 ];
 
