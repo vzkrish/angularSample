bb22c79556ea6d61cffbc478c2857ce9f20d3e45
vzkrish <krajubraju@gmail.com>
2018-08-23 17:53:11 +0530

    Create Home, Login, Contact Component

--- spa/src/app/app-routing.module.ts
+++ spa/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', redirectTo: 'home', pathMatch: 'full'},
+{path: 'login', component: LoginComponent},
+{path: 'home', component: HomeComponent},
+{path: 'contact', component: ContactComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
